0dfa34424bf9e092b9a6973cb2ad1976c7f35f7c
Raniere Silva <raniere@rgaiacs.com>
2020-05-13 18:55:21 -0300

    Reduce number of gray levels to 8 in glcm

--- pythia/glcm.py
+++ pythia/glcm.py
@@ -2,7 +2,7 @@
 import numpy as np
 from skimage.feature import greycomatrix
 
-def hist(image):
+def hist(image, number_gray_levels=8):
     """Create histogram"""
     # As the output matrix is at least levels x levels,
     # it might be preferable to use binning of the input image
@@ -13,14 +13,18 @@ def hist(image):
     binned = np.uint8(
         np.digitize(
             image,
-            np.arange(0, 256, 8)
+            np.arange(
+                0,  # Start of interval
+                256,  # End of interval
+                256 / number_gray_levels  # Spacing between values
+            )
         )
     ) - 1
 
     glcm_image = greycomatrix(
         binned,
         [1],  # distances
-        [0]  # angles
+        [0],  # angles
+        number_gray_levels
     )
     return glcm_image[:, :, 0, 0].ravel()
-    
